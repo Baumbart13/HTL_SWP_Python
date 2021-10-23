7ecc0a82cb0309e999ecd908af933465ec23296a
Thomas Kefer <tkefer@tsn.at>
2021-10-23 15:48:23 +0200

    added output

--- src/fastArrayFill.py
+++ src/fastArrayFill.py
@@ -1,9 +1,17 @@
 import datetime
 import time
 
+# for-loop needs  46.87406945228577
+# doing "[value]*noElements":  1.441084384918213
+# appending the elements in a for-loop:  49.3338360786438
+# extending the elements in a for-loop:  67.87569308280945
+# using "map":  62.11240601539612
+
 NoElements = 500000000 # allocating about 4Gigs of memory
 
 def runThis():
+    print('Measuring times, that are needed to fill a container with 1 specific value')
+
     start = time.time()
     arr = [int]*NoElements
     for i in range(0, NoElements):
