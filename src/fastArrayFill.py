5be3660197fcc6d60166a4f7e82222677f4f4eb5
Thomas Kefer <tkefer@tsn.at>
2021-11-19 11:58:01 +0100

    Reformatted code

--- src/fastArrayFill.py
+++ src/fastArrayFill.py
@@ -6,49 +6,51 @@ import time
 # extending the elements in a for-loop:  67.87569308280945
 # using "map":  62.11240601539612
 
-NoElements = 500000000 # allocating about 4Gigs of memory
+NoElements = 500000000  # allocating about 4Gigs of memory
+
 
 def runThis():
     print('Measuring times, that are needed to fill a container with 1 specific value')
 
     start = time.time()
-    arr = [int]*NoElements
+    arr = [int] * NoElements
     for i in range(0, NoElements):
         arr[i] = 3
     end = time.time()
-    del(arr)
-    print('for-loop needs ', end-start)
+    del (arr)
+    print('for-loop needs ', end - start)
 
     start = time.time()
-    arr = [3]*NoElements
+    arr = [3] * NoElements
     end = time.time()
-    del(arr)
-    print('doing "[value]*noElements": ', end-start)
+    del (arr)
+    print('doing "[value]*noElements": ', end - start)
 
     arr = []
     start = time.time()
     for i in range(0, NoElements):
         arr.append(3)
     end = time.time()
-    del(arr)
-    print('appending the elements in a for-loop: ', end-start)
+    del (arr)
+    print('appending the elements in a for-loop: ', end - start)
 
     arr = []
     start = time.time()
     for i in range(0, NoElements):
         arr.extend([3])
     end = time.time()
-    del(arr)
-    print('extending the elements in a for-loop: ', end-start)
+    del (arr)
+    print('extending the elements in a for-loop: ', end - start)
 
-    arr = [int]*NoElements
+    arr = [int] * NoElements
     start = time.time()
     arr = list(map(lambda x: 3, arr))
     end = time.time()
-    del(arr)
-    print('using "map": ', end-start)
+    del (arr)
+    print('using "map": ', end - start)
 
     print('done')
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     runThis()
