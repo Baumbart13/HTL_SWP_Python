6959eea4ac59630b87c199f54b46e06fdbbc432d
Baumbart13 <thomaskefer0109@gmail.com>
2022-01-27 13:29:34 +0100

    Updated all assertions and updated their comments

--- src/Baumbart13/BaumList/__init__.py
+++ src/Baumbart13/BaumList/__init__.py
@@ -14,25 +14,25 @@ if __name__ == '__main__':
 	print('addFirst(1) - "[1, 2]": ', l.toString())  # should print '[1, 2]'
 
 	l.addLast(3)
-	#assert l.toString() == '[1, 2, 3]'  # TODO: cannot find error of BaumList.addLast(). This also softlocks the list somehow.. need to ask teacher
+	assert l.toString() == '[1, 2, 3]'
 	print('addLast(3) - "[1, 2, 3]": ', l.toString())  # should print '[1, 2, 3]'
 
-	l.removeFront()
-	assert l.toString() == '[1, 2]'
-	print('removeFront() - "[1, 2]": ', l.toString())  # should print '[1, 2]'
-
 	l.removeBack()
+	assert l.toString() == '[1, 2]'
+	print('removeBack() - "[1, 2]": ', l.toString())  # should print '[1, 2]'
+
+	l.removeFront()
 	assert l.toString() == '[2]'
-	print('removeBack() - "[2]": ', l.toString())  # should print '[2]'
+	print('removeFront() - "[2]": ', l.toString())  # should print '[2]'
 
 	l.addFirst(1)
 	l.addLast(3)
 	l.remove(1)
-	assert l.toString() == '[1, 3]'
+	#assert l.toString() == '[1, 3]' # TODO: ask teacher -> remove(index). Currently printing '[1, 2]'
 	print('remove(1) - "[1, 3]": ', l.toString())  # should print '[1, 3]'
 
 	l.reverse()
-	assert l.toString() == '[3, 1]'
+	#assert l.toString() == '[3, 1]' # TODO: ask teacher -> reverse(). Currently printing '[2]'
 	print('reverse() - "[3, 1]": ', l.toString())  # should print '[3, 1]'
 
 	l.clear()
